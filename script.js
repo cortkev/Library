e1ac220aafe52a0504f1f41b3330138443bf35e9
Kevin <kcortes1398@gmail.com>
2022-11-30 18:39:34 -0600

    add delete function

--- script.js
+++ script.js
@@ -1,46 +1,106 @@
-// let myLibrary = [];
+const title = document.getElementById("title").value;
+const author = document.getElementById("author").value;
+const pages = document.getElementById("pages").value;
+const read = document.getElementById("read").checked;
+const bookContainer = document.getElementById('book-container');
 
-// function addBookToLibrary(){
-//     let bookName = prompt("Please enter your name", "Harry Potter");
-    
-//     if (bookName != null) {
-//         myLibrary += bookName;
-//     }
+let myLibrary = [];
 
-//     // for(let i = 0; i <= myLibrary.lengthl; i++{
-//     //     document.getElementById("demo").innerHTML =
-//     // "Books: " + myLibrary + ", ";
-//     // }
-// }
-
-// function Book(title, author, pages, read){
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = read;
-
-//     this.info = function(){
-//         return title + " by " + author + ", " + pages + ", " + read;    
-//     }
-// }
-
-// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet");
-// console.log(theHobbit.info());
-
-// for(let i = 0; i < myLibrary.length; i++){
-//     console.log(myLibrary[i]);
-// }
-
-
+//Book constructor
+function Book(title, author, pages, read){
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
 
 function addBookToLibrary(){
-    var div = document.createElement("div");
-    div.className = "cards";
-    div.style.width = "250px";
-    div.style.height = "250px";
-    div.style.background = "red";
-    div.style.color = "white";
-    div.innerHTML = "Hello";
+    const book = new Book(this.title.value, this.author.value, this.pages.value, this.read.checked);
+    myLibrary.push(book);
+    //console.log(myLibrary);
+    clearForm();
+    giveBookId();
+    createCard(book);
+    console.log(myLibrary);
+    
+}
 
-    document.getElementById('book-container').appendChild(div);
+function giveBookId(){
+  let i = 0;
+    myLibrary.map(n => {
+      n['id'] = i;
+      i++;
+    })
+    const index = myLibrary.map(book => book.id);
+    const bookIndex = index.slice(-1)[0];
+    console.log(bookIndex);
+    return bookIndex
+}
+
+document.getElementById("button").addEventListener("click", function(event){
+    event.preventDefault()
+  });
+
+// clear form after every submission
+function clearForm(){
+  this.title.value = '';
+  this.author.value = '';
+  this.pages.value = '';
+  this.read.checked = false;
+}
+
+function createCard(book){
+  giveBookId();
+  const div = document.createElement('div');
+  div.className = 'cards';
+  div.setAttribute('id', giveBookId());
+
+  const cardTitle = document.createElement('h2');
+  cardTitle.innerHTML = book.title + " " + div.id;
+  div.append(cardTitle);
+
+  const cardAuthor = document.createElement('h3');
+  cardAuthor.innerHTML = " by " +  book.author;
+  div.append(cardAuthor);
+
+  const cardPages = document.createElement('h3');
+  cardPages.innerHTML = book.pages;
+  div.append(cardPages);
+
+  const removeBtn = document.createElement('button');
+  removeBtn.className = 'remove-button';
+  removeBtn.setAttribute('onclick', 'removeButton(this)');
+  
+  const removeIcon = document.createElement('img');
+  removeIcon.setAttribute('src', 'images/deleteButton.svg');
+  removeIcon.className = 'remove-icon';
+  removeBtn.append(removeIcon);
+  div.append(removeBtn);
+
+  bookContainer.append(div);
+
+ // console.log(index);
+}
+
+//index = position in array
+function removeButton(e){
+  const selectedBookIndex = e.parentElement.id;
+  e.parentElement.remove();
+  //myLibrary = myLibrary.filter(x => x !== selectedBookIndex);
+  console.log(selectedBookIndex);
+  myLibrary.splice(selectedBookIndex, 1)
+  giveBookId();
+  
+  console.log(myLibrary);
+  //need to remove book from array
+}
+
+
+
+
+function displayBooks(){
+  myLibrary.forEach(function (book, index){
+    createCard(book, index);
+    console.log(index);
+  });
 }
